10c06ad42253b2f892b8a4f04ece1d20f7ddb70f
kcsamm <kcsamm11@gmail.com>
2022-12-02 22:25:25 +0100

    Included imports in lead.go file
    Created GetLeads function

--- lead/lead.go
+++ lead/lead.go
@@ -1,7 +1,10 @@
 package lead
 
 import (
-
+	"github.com/KING-SAMM/go-fiber-crm-basic/database"
+	"github.com/jinzhu/gorm"
+	_"github.com/jinzhu/gorm/dialects/sqlite"
+	"github.com/gofiber/fiber"
 )
 
 type Lead struct {
@@ -10,4 +13,24 @@ type Lead struct {
 	Company	string
 	Email	string
 	Phone	int
+}
+
+func GetLeads(c *fiber.Ctx) {
+
+}
+
+func GetLead(c *fiber.Ctx) {
+	id := c.Params("id")
+	db := database.DBConn
+	var lead lead 
+	db.Find(&lead, id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx) {
+
+}
+
+func DeleteLead(c *fiber.Ctx) {
+
 }
